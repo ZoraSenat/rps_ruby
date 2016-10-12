fb42c186b4778f0118702cd75287a2c9fd91034f
zora <ssh>
2016-10-11 20:25:32 -0500

    Version 4
    Allow for more flexible input.
    Let "r", "R", "2" all work to play "rock."
    Let "p", "P", "1" all work to play "paper."
    Let "s", "S, "3" all work to play "scissors."
    Let "q", "Q", "exit", "ExiT", "e" all work to "quit."

--- rps.rb
+++ rps.rb
@@ -6,6 +6,7 @@ computer_wins = 0
 while human_move.downcase != "quit"
 
   computer_options =["rock","paper","sissors"]
+  human_options = ["rock","paper","sissors","r","p","s","1","2","3"]
 
   puts "Welcome to Rock, Paper, Sissors!"
 
@@ -13,19 +14,19 @@ while human_move.downcase != "quit"
 
   human_move = gets.chomp.downcase #downcasing input eliminates case sensitivity
 
-  if computer_options.include?(human_move) == false
+  if human_options.include?(human_move) == false
     puts  "That doesn't appear to be a valid move..."
   else
     computer_move = computer_options.sample
     puts computer_move
-    if (human_move == "rock" and computer_move == "sissors") or (human_move == "sissors" and computer_move == "paper") or (human_move == "paper" and computer_move == "rock")
+    if ((human_move == "rock" or human_move[0] == "r" or human_move == "1") and computer_move == "sissors")  or ((human_move == "paper" or human_move[0] == "p" or human_move == "2") and computer_move == "rock") or ((human_move == "sissors" or human_move[0] == "s" or human_move == "3") and computer_move == "paper")
       puts "You Win!"
       human_wins = human_wins + 1
     else
-      if (human_move == computer_move)
+      if human_move[0] == computer_move[0]
         puts "It's a tie!"
       else
-        if computer_options.include?(human_move)
+        if human_options.include?(human_move)
           puts "You Lose!"
           computer_wins = computer_wins + 1
         else
