9c7d6eb0ab0e112e08fc1a1b348447d92678efea
zora <ssh>
2016-10-11 21:59:33 -0500

    Final Version
    spell check :)

--- rps.rb
+++ rps.rb
@@ -5,10 +5,10 @@ computer_wins = 0
 
 while human_move.downcase != "quit"
 
-  computer_options =["rock","paper","sissors"]
-  human_options = ["rock","paper","sissors","r","p","s","1","2","3"]
+  computer_options =["rock","paper","scissors"]
+  human_options = ["rock","paper","scissors","r","p","s","1","2","3"]
 
-  puts "Welcome to Rock, Paper, Sissors!"
+  puts "Welcome to Rock, Paper, Scissors!"
 
   puts "Your move: rock, paper, or scissors?"
 
@@ -19,7 +19,7 @@ while human_move.downcase != "quit"
   else
     computer_move = computer_options.sample
     puts computer_move
-    if ((human_move == "rock" or human_move[0] == "r" or human_move == "1") and computer_move == "sissors")  or ((human_move == "paper" or human_move[0] == "p" or human_move == "2") and computer_move == "rock") or ((human_move == "sissors" or human_move[0] == "s" or human_move == "3") and computer_move == "paper")
+    if ((human_move == "rock" or human_move[0] == "r" or human_move == "1") and computer_move == "scissors")  or ((human_move == "paper" or human_move[0] == "p" or human_move == "2") and computer_move == "rock") or ((human_move == "scissors" or human_move[0] == "s" or human_move == "3") and computer_move == "paper")
       puts "You Win!"
       human_wins = human_wins + 1
     else
